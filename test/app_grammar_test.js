4ead15dc1f428ca52a2628b9e93b54b76971785a
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-01 11:07:02 -0800

    Add support for trigger and action schemas
    This unfortunately means that compilation has to be async now,
    because we need to perform IO to retrieve the schemas from
    ThingPedia.
    
    The code is significantly more convoluted now, although the logic
    is mostly the same.

--- test/app_grammar_test.js
+++ test/app_grammar_test.js
@@ -4,10 +4,58 @@ const fs = require('fs');
 const AppCompiler = require('../lib/compiler');
 const AppGrammar = require('../lib/grammar');
 
+var _mockSchemaRetriever = {
+    _schema: {
+        "twitter": {
+            "triggers": {
+                "source": ["String","Array(String)","Array(String)","String","String","Boolean"],
+            },
+            "actions": {
+                "sink": ["String"]
+            }
+        },
+        "linkedin": {
+            "triggers": {
+                "profile": ["String","String","String","String","Any","String"],
+            },
+            "actions": {},
+        },
+        "sabrina": {
+            "triggers": {
+                "listen": ["String"],
+            },
+            "actions": {
+                "say": ["String"]
+            },
+        },
+        "test": {
+            "triggers": {
+                "source": ["Number"],
+            },
+            "actions": {}
+        },
+        "scale": {
+            "triggers": {
+                "source": ["Date","Measure(kg)"],
+            },
+            "actions": {}
+        }
+    },
+
+    getSchema: function(kind) {
+        if (kind in this._schema)
+            return Q.delay(1).then(function() {
+                return this._schema[kind];
+            }.bind(this));
+        else
+            return Q.reject(new Error("No such schema " + kind));
+    }
+};
+
 function parserTest() {
     var code = fs.readFileSync('./test/sample.apps').toString('utf8').split('====');
 
-    code.forEach(function(code) {
+    Q.all(code.map(function(code) {
         try {
             var ast = AppGrammar.parse(code);
         } catch(e) {
@@ -17,22 +65,24 @@ function parserTest() {
             return;
         }
 
-        try {
+        return Q.try(function() {
             var compiler = new AppCompiler();
+            //compiler.setSchemaRetriever(_mockSchemaRetriever);
 
-            compiler.compileProgram(ast);
-            /*compiler.rules.forEach(function(r, i) {
-                console.log('Rule ' + (i+1));
-                console.log('Inputs', r.inputs);
-                console.log('Output', r.output);
-            });*/
-        } catch(e) {
+            return compiler.compileProgram(ast).then(function() {
+                /*compiler.rules.forEach(function(r, i) {
+                    console.log('Rule ' + (i+1));
+                    console.log('Inputs', r.inputs);
+                    console.log('Output', r.output);
+                });*/
+            });
+        }).catch(function(e) {
             console.log('Compilation failed');
             console.log(code);
             console.log(e.stack);
             return;
-        }
-    });
+        });
+    }));
 }
 
 parserTest();
