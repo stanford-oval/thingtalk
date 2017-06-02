8b2689501a276b8420cbff6a2369a9d6f2bb4035
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-06-02 13:59:59 -0700

    Fix tests

--- test/mock_schema_delegate.js
+++ test/mock_schema_delegate.js
@@ -4,27 +4,37 @@ module.exports = {
             "triggers": {
                 "timer": {
                     args: ["interval"],
-                    types: ["Measure(ms)"]
+                    types: ["Measure(ms)"],
+                    required: [true],
+                    is_input: [true],
                 },
                 "at": {
                     args: ["time"],
-                    types: ["Time"]
+                    types: ["Time"],
+                    required: [true],
+                    is_input: [true],
                 }
             },
             "queries": {
                 "get_time": {
                     args: ["time"],
-                    types: ["Date"]
+                    types: ["Date"],
+                    required: [false],
+                    is_input: [false],
                 }
             },
             "actions": {
                 "notify" : {
                     args: ["message"],
-                    types: ["String"]
+                    types: ["String"],
+                    required: [true],
+                    is_input: [true],
                 },
                 "debug_log": {
                     args: ["message"],
-                    types: ["String"]
+                    types: ["String"],
+                    required: [true],
+                    is_input: [true],
                 }
             }
         },
@@ -33,7 +43,9 @@ module.exports = {
             "queries": {
                 "get_snapshot": {
                     args: ["picture_url"],
-                    types: ["Picture"]
+                    types: ["Picture"],
+                    required: [false],
+                    is_input: [false],
                 }
             },
             "actions": {}
@@ -43,7 +55,9 @@ module.exports = {
             "queries": {
                 "search_videos": {
                     args: ["query", "video_url"],
-                    types: ["String", "Entity(tt:url)"]
+                    types: ["String", "Entity(tt:url)"],
+                    required: [true,false],
+                    is_input: [true,false],
                 }
             },
             "actions": {}
@@ -52,13 +66,17 @@ module.exports = {
             "triggers": {
                 "receive_sms": {
                     args: ["from", "body"],
-                    types: ["Entity(tt:phone_number)", "String"]
+                    types: ["Entity(tt:phone_number)", "String"],
+                    required: [false,false],
+                    is_input: [false,false],
                 }
             },
             "actions": {
                 "send_sms": {
                     args: ["to", "body"],
-                    types: ["Entity(tt:phone_number)", "String"]
+                    types: ["Entity(tt:phone_number)", "String"],
+                    required: [true,true],
+                    is_input: [true,true],
                 }
             },
             "queries": {}
@@ -69,7 +87,9 @@ module.exports = {
             "queries": {
                 "get_latest": {
                     args: ["arg1", "arg2", "picture_url"],
-                    types: ["String", "String", "Entity(tt:picture)"]
+                    types: ["String", "String", "Entity(tt:picture)"],
+                    required: [false,false,false],
+                    is_input: [false,false,false],
                 }
             }
         },
@@ -78,16 +98,47 @@ module.exports = {
                 "source": {
                     args: ["text", "hashtags", "urls", "from", "inReplyTo", "__reserved"],
                     types: ["String","Array(String)","Array(String)","String","String","Boolean"],
+                    required: [false,false,false,false,false,false],
+                    is_input: [false,false,false,false,false,false],
                 }
             },
             "actions": {
                 "sink": {
                     args: ["status"],
-                    types: ["String"]
+                    types: ["String"],
+                    required: [true],
+                    is_input: [true],
                 },
                 "post_picture": {
                     args: ["caption", "picture_url"],
-                    types: ["String", "Entity(tt:picture)"]
+                    types: ["String", "Entity(tt:picture)"],
+                    required: [true,true],
+                    is_input: [true,true],
+                }
+            },
+            "queries": {}
+        },
+        "org.twitter": {
+            "triggers": {
+                "source": {
+                    args: ["text", "hashtags", "urls", "from", "inReplyTo", "__reserved"],
+                    types: ["String","Array(String)","Array(String)","String","String","Boolean"],
+                    required: [false,false,false,false,false,false],
+                    is_input: [false,false,false,false,false,false],
+                }
+            },
+            "actions": {
+                "sink": {
+                    args: ["status"],
+                    types: ["String"],
+                    required: [true],
+                    is_input: [true],
+                },
+                "post_picture": {
+                    args: ["caption", "picture_url"],
+                    types: ["String", "Entity(tt:picture)"],
+                    required: [true,true],
+                    is_input: [true,true],
                 }
             },
             "queries": {}
@@ -96,7 +147,9 @@ module.exports = {
             "triggers": {
                 "weather": {
                     args: ["location", "temperature"],
-                    types: ["Location", "Measure(C)"]
+                    types: ["Location", "Measure(C)"],
+                    required: [true,false],
+                    is_input: [true,false],
                 }
             },
             "actions": {},
@@ -106,7 +159,9 @@ module.exports = {
             "triggers": {
                 "incomingmessage": {
                     args: ["type", "message"],
-                    types: ["Enum(text,picture)", "String"]
+                    types: ["Enum(text,picture)", "String"],
+                    required: [false,false],
+                    is_input: [false,false],
                 }
             },
             "actions": {},
@@ -116,7 +171,9 @@ module.exports = {
             "triggers": {
                 "source": {
                     args: ["value"],
-                    types: ["Number"]
+                    types: ["Number"],
+                    required: [false],
+                    is_input: [false],
                 }
             },
             "actions": {},
@@ -126,13 +183,17 @@ module.exports = {
             "triggers": {
                 "temperature": {
                     args: ["time", "temperature"],
-                    types: ["Date", "Measure(C)"]
+                    types: ["Date", "Measure(C)"],
+                    required: [false,false],
+                    is_input: [false,false],
                 }
             },
             "actions": {
                 "set_target_temperature": {
                     args: ["value"],
-                    types: ["Measure(C)"]
+                    types: ["Measure(C)"],
+                    required: [true],
+                    is_input: [true],
                 }
             },
             "queries": {}
@@ -142,6 +203,8 @@ module.exports = {
                 "new_comic": {
                     args: ["title", "link", "picture_url"],
                     types: ["String", "Entity(tt:url)", "Entity(tt:picture)"],
+                    required: [false,false,false],
+                    is_input: [false,false,false],
                 }
             },
             "actions": {},
