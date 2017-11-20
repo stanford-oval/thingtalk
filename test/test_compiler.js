41435901b6468a65b28da4256b6889d773591cd8
Giovanni Campagna <gcampagn@cs.stanford.edu>
2017-11-20 15:54:05 -0800

    Change the signature of @remote.send/receive
    Pass the program id instead of an opaque flow identifier; this way,
    the protocol handlers can associate the InstallProgram message
    with the corresponding Data and End messages.

--- test/test_compiler.js
+++ test/test_compiler.js
@@ -578,9 +578,9 @@ const TEST_CASES = [
 
     [`AlmondGenerated() {
     class @__dyn_0 extends @org.thingpedia.builtin.thingengine.remote {
-        action send (in req __principal : Entity(tt:contact), in req __token : Entity(tt:flow_token), in req __kindChannel : Entity(tt:function));
+        action send (in req __principal : Entity(tt:contact), in req __program_id : Entity(tt:program_id), in req __flow : Number, in req __kindChannel : Entity(tt:function));
     }
-    @twitter.source()  => @__dyn_0.send(__principal="mock-account:12345678"^^tt:contact("me"), __token="XXXXXXXX"^^tt:flow_token, __kindChannel=$event.type) ;
+    @twitter.source()  => @__dyn_0.send(__principal="mock-account:12345678"^^tt:contact("me"), __program_id=$event.program_id, __flow=0, __kindChannel=$event.type) ;
 }`, [`"use strict";
   let _t_0;
   let _t_1;
@@ -593,6 +593,7 @@ const TEST_CASES = [
   let _t_8;
   let _t_9;
   let _t_10;
+  let _t_11;
   try {
     _t_0 = new Array(6);
     _t_1 = yield env.invokeTrigger(0, _t_0, false);
@@ -605,12 +606,14 @@ const TEST_CASES = [
         _t_4 = _t_2[1];
         _t_5 = _t_2[2];
         try {
-          _t_6 = new Array(3);
+          _t_6 = new Array(4);
           _t_7 = new __builtin.Entity("mock-account:12345678", "me");
           _t_6[0] = _t_7;
-          _t_8 = new __builtin.Entity("XXXXXXXX", null);
+          _t_8 = env.program_id;
           _t_6[1] = _t_8;
-          _t_6[2] = _t_3;
+          _t_9 = 0;
+          _t_6[2] = _t_9;
+          _t_6[3] = _t_3;
           yield env.invokeAction(1, _t_6);
         } catch(_exc_) {
           env.reportError("Failed to invoke action", _exc_);
@@ -622,9 +625,9 @@ const TEST_CASES = [
     env.reportError("Failed to invoke trigger", _exc_);
   }
   try {
-    _t_9 = new __builtin.Entity("mock-account:12345678", "me");
-    _t_10 = new __builtin.Entity("XXXXXXXX", null);
-    yield env.sendEndOfFlow(_t_9, _t_10);
+    _t_10 = new __builtin.Entity("mock-account:12345678", "me");
+    _t_11 = 0;
+    yield env.sendEndOfFlow(_t_10, _t_11);
   } catch(_exc_) {
     env.reportError("Failed to signal end-of-flow", _exc_);
   }`]],
@@ -790,57 +793,11 @@ const TEST_CASES = [
     env.reportError("Failed to invoke trigger", _exc_);
   }`]],
 
-  [`"1234"^^tt:contact_group : AlmondGenerated() {
-    class @__dyn_0 extends @org.thingpedia.builtin.thingengine.remote {
-        trigger receive (in req __principal : Entity(tt:contact), in req __token : Entity(tt:flow_token), out __kindChannel : Entity(tt:function), out interval : Measure(ms));
-    }
-    @__dyn_0.receive(__principal="mock-account:12345678"^^tt:contact("me"), __token="XXXXXXXX"^^tt:flow_token)  => @security-camera.set_power(power=enum(on)) ;
-}`, [`"use strict";
-  let _t_0;
-  let _t_1;
-  let _t_2;
-  let _t_3;
-  let _t_4;
-  let _t_5;
-  let _t_6;
-  let _t_7;
-  let _t_8;
-  let _t_9;
-  try {
-    _t_0 = new Array(4);
-    _t_1 = new __builtin.Entity("mock-account:12345678", "me");
-    _t_0[0] = _t_1;
-    _t_2 = new __builtin.Entity("XXXXXXXX", null);
-    _t_0[1] = _t_2;
-    _t_3 = yield env.invokeTrigger(0, _t_0, false);
-    {
-      let _iter_tmp = yield _t_3.next();
-      while (!_iter_tmp.done) {
-        _t_4 = _iter_tmp.value;
-        env.clearGetCache();
-        _t_5 = _t_4[0];
-        _t_6 = _t_4[1];
-        _t_7 = _t_4[2];
-        try {
-          _t_8 = new Array(1);
-          _t_9 = "on";
-          _t_8[0] = _t_9;
-          yield env.invokeAction(1, _t_8);
-        } catch(_exc_) {
-          env.reportError("Failed to invoke action", _exc_);
-        }
-        _iter_tmp = yield _t_3.next();
-      }
-    }
-  } catch(_exc_) {
-    env.reportError("Failed to invoke trigger", _exc_);
-  }`]],
-
     [`Main() {
     class @__dyn_0 extends @org.thingpedia.builtin.thingengine.remote {
-        action send (in req __principal : Entity(tt:contact_group), in req __token : Entity(tt:flow_token), in req __kindChannel : Entity(tt:function), in opt interval : Measure(ms));
+        action send (in req __principal : Entity(tt:contact_group), in req __program_id : Entity(tt:program_id), in req __flow : Number, in req __kindChannel : Entity(tt:function), in opt interval : Measure(ms));
     }
-    @builtin.timer(interval=10s)  => @__dyn_0.send(__principal="1234"^^tt:contact_group, __token="XXXXXXXX"^^tt:flow_token, __kindChannel=$event.type, interval=10s) ;
+    @builtin.timer(interval=10s)  => @__dyn_0.send(__principal="1234"^^tt:contact_group, __program_id=$event.program_id, __flow=0, __kindChannel=$event.type, interval=10s) ;
 }`, [`"use strict";
   let _t_0;
   let _t_1;
@@ -855,6 +812,7 @@ const TEST_CASES = [
   let _t_10;
   let _t_11;
   let _t_12;
+  let _t_13;
   try {
     _t_0 = new Array(1);
     _t_1 = 10000;
@@ -869,14 +827,16 @@ const TEST_CASES = [
         _t_5 = _t_3[1];
         _t_6 = _t_3[2];
         try {
-          _t_7 = new Array(4);
+          _t_7 = new Array(5);
           _t_8 = new __builtin.Entity("1234", null);
           _t_7[0] = _t_8;
-          _t_9 = new __builtin.Entity("XXXXXXXX", null);
+          _t_9 = env.program_id;
           _t_7[1] = _t_9;
-          _t_7[2] = _t_4;
-          _t_10 = 10000;
-          _t_7[3] = _t_10;
+          _t_10 = 0;
+          _t_7[2] = _t_10;
+          _t_7[3] = _t_4;
+          _t_11 = 10000;
+          _t_7[4] = _t_11;
           yield env.invokeAction(1, _t_7);
         } catch(_exc_) {
           env.reportError("Failed to invoke action", _exc_);
@@ -888,18 +848,18 @@ const TEST_CASES = [
     env.reportError("Failed to invoke trigger", _exc_);
   }
   try {
-    _t_11 = new __builtin.Entity("1234", null);
-    _t_12 = new __builtin.Entity("XXXXXXXX", null);
-    yield env.sendEndOfFlow(_t_11, _t_12);
+    _t_12 = new __builtin.Entity("1234", null);
+    _t_13 = 0;
+    yield env.sendEndOfFlow(_t_12, _t_13);
   } catch(_exc_) {
     env.reportError("Failed to signal end-of-flow", _exc_);
   }`]],
 
-  [`"1234"^^tt:contact : AlmondGenerated() {
+  [`"1234"^^tt:contact_group : AlmondGenerated() {
     class @__dyn_0 extends @org.thingpedia.builtin.thingengine.remote {
-        action send (in req __principal : Entity(tt:contact_group), in req __token : Entity(tt:flow_token), in req __kindChannel : Entity(tt:function), in opt start_time : Date, in opt has_sound : Boolean, in opt has_motion : Boolean, in opt has_person : Boolean, in opt picture_url : Entity(tt:picture));
+        trigger receive (in req __principal : Entity(tt:contact_group), in req __program_id : Entity(tt:program_id), in req __flow : Number, out __kindChannel : Entity(tt:function), out interval : Measure(ms));
     }
-    @security-camera.new_event()  => @__dyn_0.send(__principal=["mock-account:12345678"^^tt:contact("me")], __token="XXXXXXXX"^^tt:flow_token, __kindChannel=$event.type) ;
+    @__dyn_0.receive(__principal=["mock-account:12345678"^^tt:contact("me")], __program_id=$event.program_id, __flow=0)  => @security-camera.set_power(power=enum(on)) ;
 }`, [`"use strict";
   let _t_0;
   let _t_1;
@@ -914,38 +874,34 @@ const TEST_CASES = [
   let _t_10;
   try {
     _t_0 = new Array(5);
-    _t_1 = yield env.invokeTrigger(0, _t_0, false);
+    _t_1 = [new __builtin.Entity("mock-account:12345678", "me")];
+    _t_0[0] = _t_1;
+    _t_2 = env.program_id;
+    _t_0[1] = _t_2;
+    _t_3 = 0;
+    _t_0[2] = _t_3;
+    _t_4 = yield env.invokeTrigger(0, _t_0, false);
     {
-      let _iter_tmp = yield _t_1.next();
+      let _iter_tmp = yield _t_4.next();
       while (!_iter_tmp.done) {
-        _t_2 = _iter_tmp.value;
+        _t_5 = _iter_tmp.value;
         env.clearGetCache();
-        _t_3 = _t_2[0];
-        _t_4 = _t_2[1];
-        _t_5 = _t_2[2];
+        _t_6 = _t_5[0];
+        _t_7 = _t_5[1];
+        _t_8 = _t_5[2];
         try {
-          _t_6 = new Array(8);
-          _t_7 = [new __builtin.Entity("mock-account:12345678", "me")];
-          _t_6[0] = _t_7;
-          _t_8 = new __builtin.Entity("XXXXXXXX", null);
-          _t_6[1] = _t_8;
-          _t_6[2] = _t_3;
-          yield env.invokeAction(1, _t_6);
+          _t_9 = new Array(1);
+          _t_10 = "on";
+          _t_9[0] = _t_10;
+          yield env.invokeAction(1, _t_9);
         } catch(_exc_) {
           env.reportError("Failed to invoke action", _exc_);
         }
-        _iter_tmp = yield _t_1.next();
+        _iter_tmp = yield _t_4.next();
       }
     }
   } catch(_exc_) {
     env.reportError("Failed to invoke trigger", _exc_);
-  }
-  try {
-    _t_9 = [new __builtin.Entity("mock-account:12345678", "me")];
-    _t_10 = new __builtin.Entity("XXXXXXXX", null);
-    yield env.sendEndOfFlow(_t_9, _t_10);
-  } catch(_exc_) {
-    env.reportError("Failed to signal end-of-flow", _exc_);
   }`]]
 ];
 
