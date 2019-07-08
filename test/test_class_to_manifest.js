34cca410114a0b1faf40b2c110c6cf73b2a0592b
Silei Xu <ray.slxu@gmail.com>
2019-07-08 16:02:20 -0700

    Include org.thingpedia.database loader

--- test/test_class_to_manifest.js
+++ test/test_class_to_manifest.js
@@ -315,7 +315,26 @@ const TEST_CASES = [
     '  #_[confirmation="bar"]\n' +
     '  #[require_filter=true]\n' +
     '  #[default_projection=["title", "description"]];\n' +
-    '}\n'
+    '}\n',
+
+    'class @foo {\n' +
+    '  import loader from @org.thingpedia.database(type=enum(sql));\n' +
+    '  import config from @org.thingpedia.config.none();\n' +
+    '}\n',
+
+    {
+      module_type: 'org.thingpedia.database',
+      kind: 'foo',
+      params: {},
+      auth: { type: 'none' },
+      queries: {},
+      actions: {},
+      version: 0,
+      types: [],
+      child_types: [],
+      category: 'data',
+      query_type: 'sql' }
+
 ];
 
 async function test(i) {
