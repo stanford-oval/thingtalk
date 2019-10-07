897922e6d13de4a03f7388665e23255efd2a4d70
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-06 18:18:36 -0700

    Implement aggregate filters && compute expression
    The last piece to have full ThingTalk query language, and also
    no more NotImplementedError in the compiler!

--- test/test_compiler.js
+++ test/test_compiler.js
@@ -5597,7 +5597,7 @@ const TEST_CASES = [
     __env.reportError("Failed to invoke query", _exc_);
   }`]],
 
-    //71 parameter passing with compound types
+    //72 parameter passing with compound types
     [`now => @org.thingpedia.test.compounds_and_inheritance.foo() => @com.twitter.post(status=compound.nestedCompound.p1);`,
     [`"use strict";
   let _t_0;
@@ -5659,7 +5659,7 @@ const TEST_CASES = [
     __env.reportError("Failed to invoke query", _exc_);
   }`]],
 
-    // 72 monitor of that stuff
+    // 73 monitor of that stuff
     [`monitor @org.thingpedia.test.compounds_and_inheritance.foo() => notify;`,
     [`"use strict";
   let _t_0;
@@ -5726,6 +5726,345 @@ const TEST_CASES = [
     }
   } catch(_exc_) {
     __env.reportError("Failed to invoke trigger", _exc_);
+  }`]],
+
+    // 74 filter() compute operator
+    [`now => compute filter(review, reviewRating.ratingValue >= 0) of @org.schema.restaurant() => notify;`,
+    [`"use strict";
+  let _t_0;
+  let _t_1;
+  let _t_2;
+  let _t_3;
+  let _t_4;
+  let _t_5;
+  let _t_6;
+  let _t_7;
+  let _t_8;
+  let _t_9;
+  let _t_10;
+  let _t_11;
+  let _t_12;
+  let _t_13;
+  let _t_14;
+  let _t_15;
+  let _t_16;
+  let _t_17;
+  let _t_18;
+  let _t_19;
+  let _t_20;
+  let _t_21;
+  let _t_22;
+  let _t_23;
+  let _t_24;
+  let _t_25;
+  let _t_26;
+  let _t_27;
+  let _t_28;
+  let _t_29;
+  let _t_30;
+  let _t_31;
+  let _t_32;
+  let _t_33;
+  let _t_34;
+  let _t_35;
+  let _t_36;
+  let _t_37;
+  let _t_38;
+  let _t_39;
+  let _t_40;
+  let _t_41;
+  let _t_42;
+  let _t_43;
+  let _t_44;
+  let _t_45;
+  try {
+    _t_0 = {};
+    _t_1 = await __env.invokeQuery("org.schema", { }, "restaurant", _t_0);
+    _t_2 = _t_1[Symbol.iterator]();
+    {
+      let _iter_tmp = await _t_2.next();
+      while (!_iter_tmp.done) {
+        _t_3 = _iter_tmp.value;
+        _t_4 = _t_3[0];
+        _t_5 = _t_3[1];
+        _t_6 = _t_5.__response;
+        _t_7 = _t_5.name;
+        _t_8 = _t_5.serveCuisine;
+        _t_9 = _t_5.priceRange;
+        _t_10 = _t_5.openingHours;
+        _t_11 = _t_5.address;
+        if (_t_11) {
+          _t_12 = _t_11.addressCountry;
+          _t_13 = _t_11.addressRegion;
+          _t_14 = _t_11.postalCode;
+          _t_15 = _t_11.streetAddress;
+          _t_16 = _t_11.addressLocality;
+        } else {
+
+        }
+        _t_17 = _t_5.aggregateRating;
+        if (_t_17) {
+          _t_18 = _t_17.ratingValue;
+          _t_19 = _t_17.reviewCount;
+        } else {
+
+        }
+        _t_20 = _t_5.review;
+        _t_21 = _t_5.telephone;
+        _t_22 = _t_5.brand;
+        if (_t_22) {
+          _t_23 = _t_22.name;
+        } else {
+
+        }
+        _t_24 = _t_5.name;
+        _t_25 = _t_5.description;
+        _t_26 = _t_5.image;
+        _t_27 = _t_5.address;
+        if (_t_27) {
+          _t_28 = _t_27.addressCountry;
+          _t_29 = _t_27.addressRegion;
+          _t_30 = _t_27.postalCode;
+          _t_31 = _t_27.streetAddress;
+          _t_32 = _t_27.addressLocality;
+        } else {
+
+        }
+        _t_33 = _t_5.geo;
+        _t_34 = _t_5.name;
+        _t_35 = _t_5.description;
+        _t_36 = _t_5.image;
+        _t_37 = _t_20.filter((_t_38) => {
+          _t_39 = _t_38.reviewRating;
+          if (_t_39) {
+            _t_40 = _t_39.ratingValue;
+          } else {
+
+          }
+          _t_41 = _t_38.datePublished;
+          _t_42 = _t_38.description;
+          _t_43 = _t_38.author;
+          _t_45 = 0;
+          _t_44 = _t_40 >= _t_45;
+          return _t_44;
+        });
+        _t_5.review = _t_37;
+        try {
+          await __env.output(String(_t_4), _t_5);
+        } catch(_exc_) {
+          __env.reportError("Failed to invoke action", _exc_);
+        }
+        _iter_tmp = await _t_2.next();
+      }
+    }
+  } catch(_exc_) {
+    __env.reportError("Failed to invoke query", _exc_);
+  }`]],
+
+    // 75 aggregate filter
+    [`now => @org.schema.restaurant(), count(review, reviewRating.ratingValue >= 0) >= 0 => notify;`,
+    [`"use strict";
+  let _t_0;
+  let _t_1;
+  let _t_2;
+  let _t_3;
+  let _t_4;
+  let _t_5;
+  let _t_6;
+  let _t_7;
+  let _t_8;
+  let _t_9;
+  let _t_10;
+  let _t_11;
+  let _t_12;
+  let _t_13;
+  let _t_14;
+  let _t_15;
+  let _t_16;
+  let _t_17;
+  let _t_18;
+  let _t_19;
+  let _t_20;
+  let _t_21;
+  let _t_22;
+  let _t_23;
+  let _t_24;
+  let _t_25;
+  let _t_26;
+  let _t_27;
+  let _t_28;
+  let _t_29;
+  let _t_30;
+  let _t_31;
+  let _t_32;
+  let _t_33;
+  let _t_34;
+  let _t_35;
+  let _t_36;
+  let _t_37;
+  let _t_38;
+  let _t_39;
+  let _t_40;
+  let _t_41;
+  let _t_42;
+  let _t_43;
+  let _t_44;
+  let _t_45;
+  let _t_46;
+  let _t_47;
+  let _t_48;
+  let _t_49;
+  try {
+    _t_0 = {};
+    _t_1 = await __env.invokeQuery("org.schema", { }, "restaurant", _t_0);
+    _t_2 = _t_1[Symbol.iterator]();
+    {
+      let _iter_tmp = await _t_2.next();
+      while (!_iter_tmp.done) {
+        _t_3 = _iter_tmp.value;
+        _t_4 = _t_3[0];
+        _t_5 = _t_3[1];
+        _t_6 = _t_5.__response;
+        _t_7 = _t_5.name;
+        _t_8 = _t_5.serveCuisine;
+        _t_9 = _t_5.priceRange;
+        _t_10 = _t_5.openingHours;
+        _t_11 = _t_5.address;
+        if (_t_11) {
+          _t_12 = _t_11.addressCountry;
+          _t_13 = _t_11.addressRegion;
+          _t_14 = _t_11.postalCode;
+          _t_15 = _t_11.streetAddress;
+          _t_16 = _t_11.addressLocality;
+        } else {
+
+        }
+        _t_17 = _t_5.aggregateRating;
+        if (_t_17) {
+          _t_18 = _t_17.ratingValue;
+          _t_19 = _t_17.reviewCount;
+        } else {
+
+        }
+        _t_20 = _t_5.review;
+        _t_21 = _t_5.telephone;
+        _t_22 = _t_5.brand;
+        if (_t_22) {
+          _t_23 = _t_22.name;
+        } else {
+
+        }
+        _t_24 = _t_5.name;
+        _t_25 = _t_5.description;
+        _t_26 = _t_5.image;
+        _t_27 = _t_5.address;
+        if (_t_27) {
+          _t_28 = _t_27.addressCountry;
+          _t_29 = _t_27.addressRegion;
+          _t_30 = _t_27.postalCode;
+          _t_31 = _t_27.streetAddress;
+          _t_32 = _t_27.addressLocality;
+        } else {
+
+        }
+        _t_33 = _t_5.geo;
+        _t_34 = _t_5.name;
+        _t_35 = _t_5.description;
+        _t_36 = _t_5.image;
+        _t_38 = _t_20.filter((_t_39) => {
+          _t_40 = _t_39.reviewRating;
+          if (_t_40) {
+            _t_41 = _t_40.ratingValue;
+          } else {
+
+          }
+          _t_42 = _t_39.datePublished;
+          _t_43 = _t_39.description;
+          _t_44 = _t_39.author;
+          _t_46 = 0;
+          _t_45 = _t_41 >= _t_46;
+          return _t_45;
+        });
+        _t_47 = "*";
+        _t_48 = __builtin.count(_t_38, _t_47);
+        _t_49 = 0;
+        _t_37 = _t_48 >= _t_49;
+        if (_t_37) {
+          try {
+            await __env.output(String(_t_4), _t_5);
+          } catch(_exc_) {
+            __env.reportError("Failed to invoke action", _exc_);
+          }
+        } else {
+
+        }
+        _iter_tmp = await _t_2.next();
+      }
+    }
+  } catch(_exc_) {
+    __env.reportError("Failed to invoke query", _exc_);
+  }`]],
+
+    // 76 distance operator
+    [`now => compute distance(geo, makeLocation(90,0,"north pole")) of @org.schema.place() => notify;`,
+    [`"use strict";
+  let _t_0;
+  let _t_1;
+  let _t_2;
+  let _t_3;
+  let _t_4;
+  let _t_5;
+  let _t_6;
+  let _t_7;
+  let _t_8;
+  let _t_9;
+  let _t_10;
+  let _t_11;
+  let _t_12;
+  let _t_13;
+  let _t_14;
+  let _t_15;
+  let _t_16;
+  let _t_17;
+  let _t_18;
+  try {
+    _t_0 = {};
+    _t_1 = await __env.invokeQuery("org.schema", { }, "place", _t_0);
+    _t_2 = _t_1[Symbol.iterator]();
+    {
+      let _iter_tmp = await _t_2.next();
+      while (!_iter_tmp.done) {
+        _t_3 = _iter_tmp.value;
+        _t_4 = _t_3[0];
+        _t_5 = _t_3[1];
+        _t_6 = _t_5.__response;
+        _t_7 = _t_5.address;
+        if (_t_7) {
+          _t_8 = _t_7.addressCountry;
+          _t_9 = _t_7.addressRegion;
+          _t_10 = _t_7.postalCode;
+          _t_11 = _t_7.streetAddress;
+          _t_12 = _t_7.addressLocality;
+        } else {
+
+        }
+        _t_13 = _t_5.geo;
+        _t_14 = _t_5.name;
+        _t_15 = _t_5.description;
+        _t_16 = _t_5.image;
+        _t_17 = new __builtin.Location(90, 0, "north pole");
+        _t_18 = __builtin.distance(_t_13, _t_17);
+        _t_5.distance = _t_18;
+        try {
+          await __env.output(String(_t_4), _t_5);
+        } catch(_exc_) {
+          __env.reportError("Failed to invoke action", _exc_);
+        }
+        _iter_tmp = await _t_2.next();
+      }
+    }
+  } catch(_exc_) {
+    __env.reportError("Failed to invoke query", _exc_);
   }`]],
 ];
 
