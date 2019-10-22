59a1d9b17c13525ee6f8f9ee8ddec9955c403508
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-21 20:43:09 -0700

    v1.9.0-alpha.2

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const Builtin = require('./lib/builtin');
  * @type {string}
  * @alias version
  */
-const version = '1.9.0-alpha.1';
+const version = '1.9.0-alpha.2';
 
 module.exports = {
     version,
