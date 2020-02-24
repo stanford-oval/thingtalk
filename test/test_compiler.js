fd389b4e31c0063622313778c75b6e3e67d9399d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-02-24 13:49:39 -0800

    Fix compiling projections
    With and without minimal projection

--- test/test_compiler.js
+++ test/test_compiler.js
@@ -774,7 +774,7 @@ const TEST_CASES = [
 
     //18
     [`monitor @com.twitter.home_timeline(), text =~ "foo" || (text =~"bar" && !(text =~ "lol")) => notify;`,
-     [`"use strict";
+     [` "use strict";
   let _t_0;
   let _t_1;
   let _t_2;
@@ -822,18 +822,18 @@ const TEST_CASES = [
         _t_0 = _t_14;
         if (_t_13) {
           _t_15 = false;
-          _t_17 = "foo";
-          _t_16 = __builtin.like(_t_7, _t_17);
+          _t_16 = true;
+          _t_18 = "bar";
+          _t_17 = __builtin.like(_t_7, _t_18);
+          _t_16 = _t_16 && _t_17;
+          _t_21 = "lol";
+          _t_20 = __builtin.like(_t_7, _t_21);
+          _t_19 = ! (_t_20);
+          _t_16 = _t_16 && _t_19;
           _t_15 = _t_15 || _t_16;
-          _t_18 = true;
-          _t_20 = "bar";
-          _t_19 = __builtin.like(_t_7, _t_20);
-          _t_18 = _t_18 && _t_19;
-          _t_23 = "lol";
+          _t_23 = "foo";
           _t_22 = __builtin.like(_t_7, _t_23);
-          _t_21 = ! (_t_22);
-          _t_18 = _t_18 && _t_21;
-          _t_15 = _t_15 || _t_18;
+          _t_15 = _t_15 || _t_22;
           if (_t_15) {
             try {
               await __env.output(String(_t_4), _t_5);
@@ -6343,6 +6343,7 @@ const TEST_CASES = [
   let _t_31;
   let _t_32;
   let _t_33;
+  let _t_34;
   _t_0 = false;
   _t_1 = Infinity;
   try {
@@ -6395,8 +6396,15 @@ const TEST_CASES = [
     _t_31 = _t_2.geo;
     _t_32 = _t_2.phone;
     _t_33 = _t_2.distance;
+    _t_34 = {};
+    _t_34.id = _t_24;
+    _t_34.link = _t_26;
+    _t_34.rating = _t_29;
+    _t_34.cuisines = _t_27;
+    _t_34.geo = _t_31;
+    _t_34.image_url = _t_25;
     try {
-      await __env.output(String(_t_3), _t_2);
+      await __env.output(String(_t_3), _t_34);
     } catch(_exc_) {
       __env.reportError("Failed to invoke action", _exc_);
     }
@@ -6466,7 +6474,63 @@ const TEST_CASES = [
     await __env.output(String(_t_18), _t_20);
   } catch(_exc_) {
     __env.reportError("Failed to invoke action", _exc_);
-  }`]]
+  }`]],
+
+    // 87 projection
+    [`now => [rating] of (@com.yelp.restaurant()) => notify;`,
+    [`"use strict";
+  let _t_0;
+  let _t_1;
+  let _t_2;
+  let _t_3;
+  let _t_4;
+  let _t_5;
+  let _t_6;
+  let _t_7;
+  let _t_8;
+  let _t_9;
+  let _t_10;
+  let _t_11;
+  let _t_12;
+  let _t_13;
+  let _t_14;
+  let _t_15;
+  let _t_16;
+  try {
+    _t_0 = {};
+    _t_1 = await __env.invokeQuery("com.yelp", { }, "restaurant", _t_0);
+    _t_2 = _t_1[Symbol.iterator]();
+    {
+      let _iter_tmp = await _t_2.next();
+      while (!_iter_tmp.done) {
+        _t_3 = _iter_tmp.value;
+        _t_4 = _t_3[0];
+        _t_5 = _t_3[1];
+        _t_6 = _t_5.__response;
+        _t_7 = _t_5.id;
+        _t_8 = _t_5.image_url;
+        _t_9 = _t_5.link;
+        _t_10 = _t_5.cuisines;
+        _t_11 = _t_5.price;
+        _t_12 = _t_5.rating;
+        _t_13 = _t_5.reviewCount;
+        _t_14 = _t_5.geo;
+        _t_15 = _t_5.phone;
+        _t_16 = {};
+        _t_16.rating = _t_12;
+        _t_16.id = _t_7;
+        try {
+          await __env.output(String(_t_4), _t_16);
+        } catch(_exc_) {
+          __env.reportError("Failed to invoke action", _exc_);
+        }
+        _iter_tmp = await _t_2.next();
+      }
+    }
+  } catch(_exc_) {
+    __env.reportError("Failed to invoke query", _exc_);
+  }
+`]],
 ];
 
 // eslint-disable-next-line prefer-arrow-callback
