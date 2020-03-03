e7a45c88f471e9d31f0bb1895567ffa4ff785b2c
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-02 17:06:26 -0800

    Fix "projection of projection" optimization
    Make sure it's not applied to "projection of compute" by mistake.

--- test/test_compiler.js
+++ test/test_compiler.js
@@ -6932,6 +6932,63 @@ const TEST_CASES = [
   } catch(_exc_) {
     __env.reportError("Failed to invoke query", _exc_);
   }`]],
+
+  [`now => [count] of (compute (count(cuisines)) of (@com.yelp.restaurant())) => notify;`,
+  [`"use strict";
+  let _t_0;
+  let _t_1;
+  let _t_2;
+  let _t_3;
+  let _t_4;
+  let _t_5;
+  let _t_6;
+  let _t_7;
+  let _t_8;
+  let _t_9;
+  let _t_10;
+  let _t_11;
+  let _t_12;
+  let _t_13;
+  let _t_14;
+  let _t_15;
+  let _t_16;
+  let _t_17;
+  try {
+    _t_0 = {};
+    _t_1 = await __env.invokeQuery("com.yelp", { }, "restaurant", _t_0, { projection: ["id", "count", "cuisines"] });
+    _t_2 = _t_1[Symbol.iterator]();
+    {
+      let _iter_tmp = await _t_2.next();
+      while (!_iter_tmp.done) {
+        _t_3 = _iter_tmp.value;
+        _t_4 = _t_3[0];
+        _t_5 = _t_3[1];
+        _t_6 = _t_5.__response;
+        _t_7 = _t_5.id;
+        _t_8 = _t_5.image_url;
+        _t_9 = _t_5.link;
+        _t_10 = _t_5.cuisines;
+        _t_11 = _t_5.price;
+        _t_12 = _t_5.rating;
+        _t_13 = _t_5.reviewCount;
+        _t_14 = _t_5.geo;
+        _t_15 = _t_5.phone;
+        _t_16 = __builtin.count(_t_10);
+        _t_5.count = _t_16;
+        _t_17 = {};
+        _t_17.id = _t_7;
+        _t_17.count = _t_16;
+        try {
+          await __env.output(String(_t_4), _t_17);
+        } catch(_exc_) {
+          __env.reportError("Failed to invoke action", _exc_);
+        }
+        _iter_tmp = await _t_2.next();
+      }
+    }
+  } catch(_exc_) {
+    __env.reportError("Failed to invoke query", _exc_);
+  }`]],
 ];
 
 // eslint-disable-next-line prefer-arrow-callback
