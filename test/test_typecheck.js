950639ed7c89b3af727ae1a7ef4de3b389f2233d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-03-26 21:53:36 -0700

    test_typecheck: make it synchronous
    Running all the tests in parallel makes it hard to debug...

--- test/test_typecheck.js
+++ test/test_typecheck.js
@@ -10,50 +10,55 @@ const _mockMemoryClient = require('./mock_memory_client');
 
 const _schemaRetriever = new SchemaRetriever(_mockSchemaDelegate, _mockMemoryClient, true);
 
-function main() {
-    var code = fs.readFileSync('./test/sample.apps').toString('utf8').split('====');
+async function main() {
+    const tests = fs.readFileSync('./test/sample.apps').toString('utf8').split('====');
 
-    return Promise.all(code.map((code) => {
+    for (let code of tests) {
         code = code.trim();
-        return AppGrammar.parseAndTypecheck(code, _schemaRetriever).then((program) => {
-            if (code.indexOf(`** typecheck: expect `) >= 0) {
-                console.error('Failed (expected error)');
-                console.error(code);
-            }
-
-            try {
-                Array.from(program.iterateSlots());
-            } catch(e) {
-                console.error('Iterate slots failed');
-                console.log('Code:');
-                console.log(code);
-                console.error('====');
-                console.error(e.stack);
-                if (process.env.TEST_MODE)
-                    throw e;
-            }
-
-            try {
-                Array.from(program.iterateSlots2());
-            } catch(e) {
-                console.error('Iterate slots failed');
-                console.log('Code:');
-                console.log(code);
-                console.error('====');
-                console.error(e.stack);
-                if (process.env.TEST_MODE)
-                    throw e;
-            }
-        }, (e) => {
+        let program;
+        try {
+            program = await AppGrammar.parseAndTypecheck(code, _schemaRetriever);
+        } catch (e) {
             if (code.indexOf(`** typecheck: expect ${e.name} **`) >= 0)
-                return;
+                continue;
             console.error('Failed');
             console.error(code);
             console.error(e);
             if (process.env.TEST_MODE)
                 throw e;
-        });
-    }));
+            continue;
+        }
+
+        if (code.indexOf(`** typecheck: expect `) >= 0) {
+            console.error('Failed (expected error)');
+            console.error(code);
+            continue;
+        }
+
+        try {
+            Array.from(program.iterateSlots());
+        } catch(e) {
+            console.error('Iterate slots failed');
+            console.log('Code:');
+            console.log(code);
+            console.error('====');
+            console.error(e.stack);
+            if (process.env.TEST_MODE)
+                throw e;
+        }
+
+        try {
+            Array.from(program.iterateSlots2());
+        } catch(e) {
+            console.error('Iterate slots failed');
+            console.log('Code:');
+            console.log(code);
+            console.error('====');
+            console.error(e.stack);
+            if (process.env.TEST_MODE)
+                throw e;
+        }
+    }
 }
 module.exports = main;
 if (!module.parent)
