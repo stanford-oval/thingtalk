34c0ad92c1aff47a2bbfe49edde7c66edfe3f7aa
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-04-03 09:15:13 -0700

    v1.10.0

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const Builtin = require('./lib/builtin');
  * @type {string}
  * @alias version
  */
-const version = '1.10.0-beta.1';
+const version = '1.10.0';
 
 module.exports = {
     version,
