fd4974e553e5c33a21d864b37c12b97fba759d1e
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-09-07 19:19:44 -0700

    Fix prettyprinting of enum types that are keywords (#386)
    Avoid an extra space

--- lib/new-syntax/pretty.ts
+++ lib/new-syntax/pretty.ts
@@ -267,7 +267,7 @@ export function prettyprint(tokens : TokenStream) : string {
         case 'extends':
         case 'join':
         case 'on':
-            if (buffer.endsWith('enum '))
+            if (buffer.endsWith('enum ') || buffer.endsWith('Enum('))
                 buffer += token;
             else
                 buffer += ' ' + token + ' ';
