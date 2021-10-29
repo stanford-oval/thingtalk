457e61e0681646fad261bdbb8c359a8296963136
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-10-29 14:24:44 -0700

    Reduce range of location equality filters to 1km

--- lib/runtime/primitive_ops.ts
+++ lib/runtime/primitive_ops.ts
@@ -95,7 +95,7 @@ function locationEquals(a : LocationLike, b : LocationLike) : boolean {
     //console.log('Comparing locations', [a,b]);
     const d = distance(a, b);
     //console.log('Distance (m): ' + d.toFixed(2));
-    return d <= 10000;
+    return d <= 2000;
 }
 
 function hasValueOf(x : unknown) : x is number|string|Date|Time|StringLike {
